b8f3f2a4437b7f8cf57713ddf8b6545c01d7e979
joshuamumbere <joshuamumbere71@gmail.com>
2024-10-31 14:00:06 +0000

    Automated deploying to render

--- Ecommerce_store/settings.py
+++ Ecommerce_store/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-4u2(y_r0e$96kbw)@qslu6d-*i07qcfodf59l26z&w0a_gqp^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
